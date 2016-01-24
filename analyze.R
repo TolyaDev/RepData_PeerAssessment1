9a43444b35010ec99a49e6f930d91a64ef2ccc41
Anatoly Vasilyev <anatoly.developer@gmail.com>
2016-01-24 19:49:22 +0300

    completed

--- analyze.R
+++ analyze.R
@@ -1,45 +1,18 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
-    echo: true
----
-
-
-## Loading and preprocessing the data
-```{r}
 library(data.table)
 library(ggplot2)
 
 data <- fread('activity.csv')
 data.sub <- na.omit(data)
 data.sub.perday <- data.sub[,.(stepssum=sum(steps)),date]
-```
 
-
-
-## What is mean total number of steps taken per day?
-```{r}
 ggplot(data.sub.perday, aes(stepssum)) + geom_histogram() + theme_bw()
 mean(data.sub.perday$stepssum)
 median(data.sub.perday$stepssum)
 
-```
-
-
-
-## What is the average daily activity pattern?
-```{r}
-
 data.sub.perinterval <- data.sub[,.(stepsmean = mean(steps), stepsmedian = median(steps)),interval]
 ggplot(data.sub.perinterval, aes(interval, stepsmean)) + geom_line() + theme_bw()
 data.sub.perinterval[which.max(data.sub.perinterval$stepsmean)]
-```
 
-
-## Imputing missing values
-```{r}
 data.na <- data[which(is.na(data$steps))]
 data.merged <- merge(data.na, data.sub.perinterval, by='interval')
 data.merged[,steps:=floor(data.merged$stepsmedian)]
@@ -47,28 +20,17 @@ data.merged[,stepsmean:=NULL]
 data.merged[,stepsmedian:=NULL]
 data.imput <- rbind(data.sub, data.merged)
 
-```
-Do these values differ from the estimates from the first part of the assignment? What is the impact of imputing missing data on the estimates of the total daily number of steps?
-
-```{r}
 data.imput.perday <- data.imput[,.(stepssum=sum(steps)),date]
 ggplot(data.imput.perday, aes(stepssum)) + geom_histogram() + theme_bw()
 mean(data.imput.perday$stepssum)
 median(data.imput.perday$stepssum)
 
-```
-
-## Are there differences in activity patterns between weekdays and weekends?
-```{r}
 Sys.setlocale("LC_TIME", "C")
 data.imput$date <- as.Date(data.imput$date, "%Y-%m-%d")
+weekdays(data.imput$date)
 weekdays.names <- c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
 data.imput[,dayofweek:=factor((weekdays(.SD$date) %in% weekdays.names), 
            levels=c(TRUE, FALSE), labels=c('weekend', 'weekday'))]
 
 data.imput.perinterval.perdayofweek <- data.imput[,.(stepsmean = mean(steps)), .(interval, dayofweek)]
 ggplot(data.imput.perinterval.perdayofweek, aes(interval, stepsmean)) + geom_line() + facet_grid(dayofweek~.) + theme_bw()
-
-
-```
-
